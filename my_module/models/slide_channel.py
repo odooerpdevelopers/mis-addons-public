2e60ae230281415b6833032305e25e6fbc4d5ba6
odooerpdevelopers <odooerpdevelopers@gmail.com>
2025-02-13 03:28:40 +0100

    fix flake8 python

--- my_module/models/slide_channel.py
+++ my_module/models/slide_channel.py
@@ -1,13 +1,13 @@
 from odoo import fields, models, tools
 
 
-
 class Channel(models.Model):
 
     _inherit = "slide.channel"
 
-    info_course = fields.Html(string="Información Curso",translate=tools.html_translate,
+    info_course = fields.Html(
+        string="Información Curso",
+        translate=tools.html_translate,
         sanitize_attributes=False,
         sanitize_form=False,
     )
-
